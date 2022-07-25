bd4528b3b70a6f5871c51ae48f8fd5542a7bac84
Noorudins <noorudins@cloudix.io>
2022-07-25 17:26:52 +0530

    stack prog init

---
+++ StackOperation/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using Stack.Operations;
+
+
+class demo : Stack_operations
+{
+    public static void Main(String[] args)
+    {
+       
+        int item;
+        demo d = new demo();
+
+        while (true)
+        {
+            Console.WriteLine("1. Push \n 2.Pop\n 3.Show Items\n 4.Top Element\n 5.Exit");
+            Console.Write("Enter the Above Operations:");
+            
+            var input = Convert.ToInt32(Console.ReadLine());
+            if(input== 5)
+            {
+                break;
+            }
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Enter An Element:");
+                    item = Convert.ToInt32(Console.ReadLine());
+                     d.Push(item);
+                     break;
+
+                case 2:
+                    d.Pop();
+                     break;
+
+                case 3:
+                   d.Display();
+                   break;
+
+                case 4:
+                    d.Peek();
+                   break;
+
+                default:
+                        Console.WriteLine("Please Select Valid Option");
+                     break;
+             }
+        }
+    }
+}
---
+++ StackOperation/ConsoleApp2/ConsoleApp2/Stack_operations.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack.Operations
+{
+    public class Stack_operations
+    {
+        const int MAX = 10;
+        int top;
+        int[] stack = new int[MAX];
+        public Stack_operations()
+        {
+            top = -1;
+        }
+        public void Push(int data)
+        {
+            if (top >= MAX)
+            {
+                Console.WriteLine("Stack Overflow");
+                
+            }
+            else
+            {
+                stack[++top] = data;
+                Console.WriteLine("Pushed Element is" +" "+ data); ;
+            }
+        }
+
+        public int  Pop()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("Poped Element :"+" "+stack[top]);
+                int value = stack[top--];
+                return value;
+            }
+        }
+
+        public void Peek()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return;
+            }
+            else
+                Console.WriteLine("The topmost element of Stack is : {0}", stack[top]);
+        }
+
+        public void Display()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return ;
+            }
+            else
+            {
+                Console.WriteLine("Items in the Stack are :");
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(stack[i]);
+                }
+            }
+        }
+    }
+
+}
