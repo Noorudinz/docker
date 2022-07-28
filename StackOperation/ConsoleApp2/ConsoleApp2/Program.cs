6b2c749d73d24991d0b95f82d5dac0892bf880a1
Noorudins <noorudins@cloudix.io>
2022-07-28 21:07:23 +0530

    console app docker containerized succesfully

--- StackOperation/ConsoleApp2/ConsoleApp2/Program.cs
+++ StackOperation/ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,44 +4,45 @@ class demo : Stack_operations
 {
     public static void Main(String[] args)
     {
+        Console.WriteLine("Hello world");
        
-        int item;
-        demo d = new demo();
+        //int item;
+        //demo d = new demo();
 
-        while (true)
-        {
-            Console.WriteLine("1. Push \n 2.Pop\n 3.Show Items\n 4.Top Element\n 5.Exit");
-            Console.Write("Enter the Above Operations:");
+        //while (true)
+        //{
+        //    Console.WriteLine("1. Push \n 2.Pop\n 3.Show Items\n 4.Top Element\n 5.Exit");
+        //    Console.Write("Enter the Above Operations:");
             
-            var input = Convert.ToInt32(Console.ReadLine());
-            if(input== 5)
-            {
-                break;
-            }
-            switch (input)
-            {
-                case 1:
-                    Console.WriteLine("Enter An Element:");
-                    item = Convert.ToInt32(Console.ReadLine());
-                     d.Push(item);
-                     break;
+        //    var input = Convert.ToInt32(Console.ReadLine());
+        //    if(input== 5)
+        //    {
+        //        break;
+        //    }
+        //    switch (input)
+        //    {
+        //        case 1:
+        //            Console.WriteLine("Enter An Element:");
+        //            item = Convert.ToInt32(Console.ReadLine());
+        //             d.Push(item);
+        //             break;
 
-                case 2:
-                    d.Pop();
-                     break;
+        //        case 2:
+        //            d.Pop();
+        //             break;
 
-                case 3:
-                   d.Display();
-                   break;
+        //        case 3:
+        //           d.Display();
+        //           break;
 
-                case 4:
-                    d.Peek();
-                   break;
+        //        case 4:
+        //            d.Peek();
+        //           break;
 
-                default:
-                        Console.WriteLine("Please Select Valid Option");
-                     break;
-             }
-        }
+        //        default:
+        //                Console.WriteLine("Please Select Valid Option");
+        //             break;
+        //     }
+        //}
     }
 }
