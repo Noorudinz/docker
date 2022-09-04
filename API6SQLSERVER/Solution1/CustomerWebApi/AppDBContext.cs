f7f4c53e46d822a7d741c38951b983c258c3d870
Noorudins <noorudins@cloudix.io>
2022-09-04 20:37:30 +0530

    dot net docker api core 6 with sqlserver

---
+++ API6SQLSERVER/Solution1/CustomerWebApi/AppDBContext.cs
@@ -0,0 +1,31 @@
+﻿using CustomerWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace CustomerWebApi
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> dbContextOptions): base(dbContextOptions)
+        {
+            try
+            {
+                var dbCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                {
+                    if(dbCreator != null)
+                    {
+                        if (!dbCreator.CanConnect()) dbCreator.Create();
+                        if (!dbCreator.HasTables()) dbCreator.CreateTables();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }    
+}
---
+++ API6SQLSERVER/Solution1/CustomerWebApi/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CustomerWebApi.Models
+{
+    [Table("Customer", Schema = "dbo")]
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("customer_id")]
+        public int CustomerId { get; set; }
+
+        [Column("customer_name")]
+        public string CustomerName { get; set; }
+
+        [Column("customer_mobile")]
+        public string MobileNumber { get; set; }
+
+        [Column("customer_email")]
+        public string Email { get; set; }
+    }
+}
